54e8bb6e0be9d9145a23046845c50c1bd22382a5
ZhaslanR <zhaslan_12@bk.ru>
2017-12-07 16:45:45 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacancie { get; set; }
+        public int Payment { get; set; }
+        public int[] DateOfGettingTheJobe { get; set; } 
+
+        public Employee(string name,int payment,int[] data)
+        {
+            Name = name;
+            Payment = payment;
+            Vacancie = Vacancies.Boss;
+            DateOfGettingTheJobe = new int[3];
+            for(int i = 0;i<3;i++)
+            {
+                DateOfGettingTheJobe[i] = data[i];
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[3] {12,09,2000};
+            Employee employee = new Employee("Zhaslan", 15000, mas);
+            Console.ReadLine();
+        }
+    }
+}
